b0d327d1580fbd6718b5a733ea1e0e60d18ef571
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-08-19 16:31:58 +0000

    adds date input

--- components/Modais/DataModal.jsx
+++ components/Modais/DataModal.jsx
@@ -12,7 +12,9 @@ import {
   TableCell,
   Input,
   Button,
+  DateInput,
 } from "@nextui-org/react";
+import { parseDate } from "@internationalized/date";
 
 export function DataModal({
   isOpen,
@@ -38,24 +40,123 @@ export function DataModal({
                   <TableColumn></TableColumn>
                 </TableHeader>
                 <TableBody>
-                  {dados.map((item, index) => (
-                    <TableRow key={index}>
-                      <TableCell className="font-semibold">
-                        {item.item}
-                      </TableCell>
-                      <TableCell>
-                        <Input
-                          disabled={!isEditing}
-                          value={item.valor}
-                          type="text"
-                          variant="underlined"
-                          onChange={(e) =>
-                            handleInputChange(index, e.target.value)
-                          }
-                        ></Input>
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                  <TableRow>
+                    <TableCell className="font-semibold">
+                      Código de Pessoa:
+                    </TableCell>
+                    <TableCell>
+                      <Input
+                        disabled={!isEditing}
+                        value={dados[0].valor}
+                        type="text"
+                        variant="underlined"
+                        onChange={(e) => handleInputChange(0, e.target.value)}
+                      />
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="font-semibold">Nome:</TableCell>
+                    <TableCell>
+                      <Input
+                        disabled={!isEditing}
+                        value={dados[1].valor}
+                        type="text"
+                        variant="underlined"
+                        onChange={(e) => handleInputChange(1, e.target.value)}
+                      />
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="font-semibold">
+                      Data de Nascimento:
+                    </TableCell>
+                    <TableCell>
+                      <DateInput
+                        variant="underlined"
+                        label={"Data de Nascimento"}
+                        disabled={!isEditing}
+                        value={parseDate(
+                          dados[2].valor.split("/").reverse().join("-"),
+                        )} // Convertendo para "yyyy-MM-dd" e usando parseDate
+                        onChange={(date) =>
+                          handleInputChange(
+                            2,
+                            date.toString(), // Convertendo de volta para string "yyyy-MM-dd"
+                          )
+                        }
+                        className="max-w-sm"
+                      />
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="font-semibold">
+                      Nome da Mãe:
+                    </TableCell>
+                    <TableCell>
+                      <Input
+                        disabled={!isEditing}
+                        value={dados[3].valor}
+                        type="text"
+                        variant="underlined"
+                        onChange={(e) => handleInputChange(3, e.target.value)}
+                      />
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="font-semibold">CPF:</TableCell>
+                    <TableCell>
+                      <Input
+                        disabled={!isEditing}
+                        value={dados[4].valor}
+                        type="text"
+                        variant="underlined"
+                        onChange={(e) => handleInputChange(4, e.target.value)}
+                      />
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="font-semibold">Telefone:</TableCell>
+                    <TableCell>
+                      <Input
+                        disabled={!isEditing}
+                        value={dados[5].valor}
+                        type="tel"
+                        variant="underlined"
+                        onChange={(e) => handleInputChange(5, e.target.value)}
+                      />
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="font-semibold">Celular:</TableCell>
+                    <TableCell>
+                      <Input
+                        disabled={!isEditing}
+                        value={dados[6].valor}
+                        type="tel"
+                        variant="underlined"
+                        onChange={(e) => handleInputChange(6, e.target.value)}
+                      />
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="font-semibold">Email:</TableCell>
+                    <TableCell>
+                      <Input
+                        disabled={!isEditing}
+                        value={dados[7].valor}
+                        type="email"
+                        variant="underlined"
+                        onChange={(e) => handleInputChange(7, e.target.value)}
+                      />
+                    </TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
               {!isEditing ? (
